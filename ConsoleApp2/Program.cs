3cd0636681c62f7df4e343e5cc66e0d72b38ac1c
Vasyl-Stashchuk <65713313+Vasyl-Stashchuk@users.noreply.github.com>
2020-06-09 13:59:34 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+   public  class Program
+    {
+       public  static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Ввести довжину ребра a = ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Square(a);
+            Plo(a);
+        }
+      public static int Square(int a)
+        {
+            int V =a*a*a;
+
+
+
+            return V;
+        }
+        public static int Plo(int a)
+        {
+            int S = a * a;
+            return S;
+        }
+    }
+}
